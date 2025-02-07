366e49c1147266ea41a0f3c015963463cc9d2a0b
Zilvinas-Jonaitis-TUS22 <k00281776@student.tus.ie>
2025-02-07 12:02:49 +0000

    Gun reload and shoot mechanic

--- Assets/Scripts/Player Related/Paint Shotgun.cs
+++ Assets/Scripts/Player Related/Paint Shotgun.cs
@@ -6,35 +6,89 @@ using UnityEngine;
 public class PaintShotgun : MonoBehaviour
 {
     public bool isPlayerBusy = false;
+    public bool reloading = false;
     public int ammoLoaded = 0;
     public int reserveAmmo = 0;
 
-    private CharacterController _controller;
-    private StarterAssetsInputs _input;
+    public Animator armsAnimator;
 
-    private void Start()
-    {
-        _controller = GetComponent<CharacterController>();
-        _input = GetComponent<StarterAssetsInputs>();
-    }
+    public CharacterController _controller;
+    public StarterAssetsInputs _input;
+
         void Update()
     {
-        if (_input.sprint)
-        {
-            isPlayerBusy = false;
-        }
         if (!isPlayerBusy && !_input.sprint)
         {
+            if (_input.shoot)
+            {
+                ShootGun();
+            }
+            if (_input.reload)
+            {
+                ReloadGun();
+            }
+        }
 
+        if (ammoLoaded == 6 || reserveAmmo == 0 || _input.sprint)
+        {
+            isPlayerBusy = false;
+            reloading = false;
+            armsAnimator.SetBool("Reloading", reloading);
         }
     }
 
     public void ReloadGun()
     {
-        isPlayerBusy = true;
+        
+        if (!reloading && reserveAmmo > 0 && ammoLoaded < 7)
+        {
+            reloading = true;
+            armsAnimator.SetBool("Reloading" ,reloading);
+            //reload gun
+        }
+        else if (ammoLoaded == 6)
+        {
+            //max ammo in gun
+        }
+        else if (reserveAmmo == 0)
+        {
+            //out of reserve ammo
+        }
+        else
+        {
+            //Cannot reload right now
+        }
     }
     public void ShootGun()
     {
+        if (ammoLoaded > 0)
+        {
+            reloading = false;
+            armsAnimator.SetTrigger("Shooting");
+            armsAnimator.SetBool("Reloading", reloading);
+        }
+        
+    }
+
+    public void LoadShell()
+    {
+        ammoLoaded ++;
+        reserveAmmo --;
+}
+
+    public void ShootShell()
+    {
+        ammoLoaded--;
+        //fire shell here
+    }
+
+    public void BecomeBusy()
+    {
         isPlayerBusy = true;
+    }
+
+    public void BecomeUnBusy()
+    {
+        isPlayerBusy = false;
     }
 }
