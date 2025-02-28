05a0ab0791be8acd14878d626bbee045b6fcccfe
Zilvinas-Jonaitis-TUS22 <k00281776@student.tus.ie>
2025-02-28 12:01:44 +0000

    Refined Animations

--- Assets/Scripts/Player Related/Inventory.cs
+++ Assets/Scripts/Player Related/Inventory.cs
@@ -9,11 +9,6 @@ public class Inventory : MonoBehaviour
     public bool shotgun = false;
     public bool grapple = false;
 
-    [Header("Item GameObjects")]
-    public GameObject[] handsGO;        // Array for hands items
-    public GameObject[] grappleGO;      // Array for grapple items
-    public GameObject[] shotgunGO;      // Array for shotgun items
-
     // Start is called before the first frame update
     void Start()
     {
@@ -25,49 +20,17 @@ public class Inventory : MonoBehaviour
     {
         if (shotgun)
         {
-            DeactivateAllItems();           // Deactivate all items
-            SetItemsActive(shotgunGO);      // Activate all shotgun items
             hands = false;
             grapple = false;
         }
         else if (grapple)
         {
-            DeactivateAllItems();           // Deactivate all items
-            SetItemsActive(grappleGO);      // Activate all grapple items
             hands = false;
             shotgun = false;
         }
         else
         {
-            DeactivateAllItems();           // Deactivate all items
-            SetItemsActive(handsGO);        // Activate all hands items
             hands = true;
-        }
-    }
-
-    // Method to deactivate all items in the arrays
-    private void DeactivateAllItems()
-    {
-        SetItemsInactive(handsGO);        // Deactivate all hands items
-        SetItemsInactive(grappleGO);      // Deactivate all grapple items
-        SetItemsInactive(shotgunGO);      // Deactivate all shotgun items
-    }
-
-    // Method to deactivate all items in the array
-    private void SetItemsInactive(GameObject[] itemArray)
-    {
-        foreach (var item in itemArray)
-        {
-            item.SetActive(false);
-        }
-    }
-
-    // Method to activate all items in the array
-    private void SetItemsActive(GameObject[] itemArray)
-    {
-        foreach (var item in itemArray)
-        {
-            item.SetActive(true);
         }
     }
 }
